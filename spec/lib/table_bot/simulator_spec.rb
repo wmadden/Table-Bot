93fec377a89f2d2189e912b3c73a9f1d213ee860
Will Madden <will.madden@playup.com>
2011-09-07 19:39:26 +1000

    Added a basic outline of the #place method.

--- spec/lib/table_bot/simulator_spec.rb
+++ spec/lib/table_bot/simulator_spec.rb
@@ -1,12 +1,31 @@
 require "spec_helper"
-
 require "table_bot/simulator"
 
 describe(TableBot::Simulator) do
   
+  its(:width)  { should == 5 }
+  its(:height) { should == 5 }
+  
   describe('#place') do
-    it "should do something" do
-      
+    context 'when location is inside world' do
+      it "should place the robot at the given location"
+      it "should give the robot the specified orientation"
+    end
+    
+    context 'when location is over north boundary' do
+      it "should fail"
+    end
+    
+    context 'when location is over south boundary' do
+      it "should fail"
+    end
+    
+    context 'when location is over east boundary' do
+      it "should fail"
+    end
+    
+    context 'when location is over west boundary' do
+      it "should fail"
     end
   end
   
